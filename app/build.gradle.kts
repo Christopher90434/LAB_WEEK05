1e2c1b3a90968ac97bc4166a0915a5d8851d92b4
Christopher90434 <christopherbryan.susanto@gmail.com>
2025-09-29 14:04:09 +0700

    "Commit No. 2 - Parse JSON with Moshi and Display Image URL"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,19 @@ android {
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -29,7 +40,12 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
-    // HANYA RETROFIT UNTUK COMMIT 1
+    // RETROFIT + MOSHI UNTUK COMMIT 2
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-scalars:2.9.0")
+    implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
+
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
---
+++ app/src/main/java/com/example/labweek05/model/CatBreedData.kt
@@ -0,0 +1,6 @@
+package com.example.labweek05.model
+
+data class CatBreedData(
+    val name: String,
+    val temperament: String
+)
